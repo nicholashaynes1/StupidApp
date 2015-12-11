8588438e43d88c3d6d2cb8d6954c1996f4b7ee59
nicholashaynes1 <nicholashaynes1@gmail.com>
2015-12-11 12:18:49 -0700

    Initial Commit

---
+++ StupidApp/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  StupidApp
+//
+//  Created by Haynes, Nicholas on 12/11/15.
+//  Copyright © 2015 Haynes, Nicholas. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
